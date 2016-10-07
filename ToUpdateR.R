0d228ee08407097864b678ce5cc7f78e186fdb83
Andrew Peters <andrew.r.peters.42@gmail.com>
2016-10-07 16:27:33 -0500

    Making sure this works after restoration

--- ToUpdateR.R
+++ ToUpdateR.R
@@ -1,8 +1,10 @@
 # installing/loading the package:
 if(!require(installr)) { 
   install.packages("installr"); require(installr)} #load / install+load installr
-
+install.packages("installr")
 # using the package:
 # this will start the updating process of your R installation.  It will check for newer versions, and
 # if one is available, will guide you through the decisions you'd need to make.
 updateR()
+
+#Testing...
