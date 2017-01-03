4b263127101b713afa479cfe309af2f0e9adbb17
ARPeters <andrew.r.peters.42@gmail.com>
2017-01-03 17:00:52 -0600

    Getting started

--- ToUpdateR.R
+++ ToUpdateR.R
@@ -5,6 +5,12 @@ install.packages("installr")
 # using the package:
 # this will start the updating process of your R installation.  It will check for newer versions, and
 # if one is available, will guide you through the decisions you'd need to make.
+
+library(installr)
+
+install.packages("stringr")
+library(stringr)
+
 updateR()
 
 #Testing...
