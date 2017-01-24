478dddd94a466b36ad6b8807352ad640cbeea21a
ARPeters <andrew.r.peters.42@gmail.com>
2017-01-24 15:28:02 -0600

    Reformatted/Rewriting OKCitiesZipCounty

--- OKCities.R
+++ OKCities.R
@@ -13,6 +13,9 @@ setwd("C:/Users/Andrew/Documents/GitHub/Utilities")
 
 ds1<-read.csv("OKCitiesZipCounty.csv")
 
+ds1$ZIP.Code<-substr(ds1$ZIP.Code, start=10, stop=14)
+colnames(ds1) <- c("zip", "type", "city", "county", "area")
+# write.csv(ds1, "dsOKCitiesZipCounty.csv")
 
 # County by Income
 # "SELECTED ECONOMIC CHARACTERISTICS 2006-2010 American Community Survey 5-Year Estimates". U.S. Census Bureau. Retrieved 2012-11-25.
@@ -22,5 +25,9 @@ ds2<-read.csv("CountyxIncome.csv")
 
 # Zip codes by income, sorta.
 # https://www.irs.gov/uac/soi-tax-stats-individual-income-tax-statistics-2014-zip-code-data-soi
-
 ds3<-read.csv("IRSIncomeTax.csv")
+
+
+# Quality estimates of Oklahoma Child Care Resources
+# http://childcarefind.okdhs.org/childcarefind/Default.aspx
+ds4<-read.csv("OKChildCare.csv")
