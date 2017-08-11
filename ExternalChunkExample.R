f835609da81ea3e0e766d07698793259bd6bac28
ARPeters <andrew.r.peters.42@gmail.com>
2017-08-11 18:28:25 -0500

    RMarkdown practice, reminders

---
+++ ExternalChunkExample.R
@@ -0,0 +1,9 @@
+####################################################
+# External code for RMarkdown practice
+####################################################
+
+#Must have a label, like so:
+
+# ---- CountyIncomeHead
+ds<-read.csv("CountyxIncome.csv")
+head(ds)
