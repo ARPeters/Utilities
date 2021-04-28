c7dd38978abdeef7514ab23336f334a44005dd66
ARPeters <Andrew-Peters@ouhsc.edu>
2021-04-28 11:10:35 -0500

    Beginning to create fake SSNs

--- link-king-names/scribe-names.R
+++ link-king-names/scribe-names.R
@@ -7,6 +7,7 @@ rm(list=ls(all=TRUE)) #Clear the memory for any variables set from any previous 
 
 # ---- load-packages -----------------------------------------------------------
 library(magrittr                , quietly=TRUE)
+library(stringr                               )
 requireNamespace("dplyr"                      )
 requireNamespace("lubridate"                  )
 requireNamespace("forcats"                    )
@@ -23,10 +24,6 @@ col_types = readr::cols_only(
     # X3             = readr::col_character(),
     # X4             = readr::col_character()
 )
-
-
-
-
 
 
 # ---- load-data ---------------------------------------------------------------
@@ -54,7 +51,7 @@ dsq2 <-
   dplyr::mutate(
     rown_o = 1:dplyr::n(),
   ) %>% 
-  dplyr::filter(rown_o <= 20 ) %>%
+  dplyr::filter(rown_o <= 90000 ) %>%
   dplyr::filter(!is.na(birth_year)) %>% 
   dplyr::group_by(rown_o) %>% 
   dplyr::mutate(
@@ -86,7 +83,38 @@ dsq2 <-
    dob         = paste(birth_year, birth_month, birth_day, sep = "-")
   )
 
+ds_ssn_test <-
+  dsq2 %>% 
+  dplyr::select(
+    rown_o
+  ) %>% 
+  dplyr::mutate(
+    area     = sample(c(1:665, 667:699, 729:899), dplyr::n(), replace = TRUE),
+    group    = sample(c(1:99)                   , dplyr::n(), replace = TRUE),
+    serial   = sample(c(1:9999)                 , dplyr::n(), replace = TRUE),
+    
+    area_3   = stringr::str_pad(area  , 3, pad = "0"),
+    group_2  = stringr::str_pad(group , 2, pad = "0"),
+    serial_4 = stringr::str_pad(serial, 4, pad = "0"),
+    
+    ssn_0    = paste(area_3, group_2, serial_4, sep = "-")
+  ) %>% 
+  dplyr::group_by(ssn_0) %>% 
+  dplyr::mutate(
+    ssn_count = dplyr::n()
+  ) %>% 
+  dplyr::ungroup() %>% 
+  dplyr::mutate(
+    not_unique = 1 < ssn_count,
+    swap       = sample(c(1:3), dplyr::n(), replace = TRUE)
+  )
 
-# TEST TEST TEST TEST
+ds_ssn_test_2 <-
+  ds_ssn_test
 
+table(table(ds_ssn_test$ssn_0))
+table(ds_ssn_test$ssn_count)
+View(head(ds_ssn_test))
 
+table(table(ds_ssn_test$ssn_0))
+View(head(ds_ssn_test))
