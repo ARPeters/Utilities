b5f1dfa53dda45d6d80e16c80b2f8d0a38c468ff
ARPeters <Andrew-Peters@ouhsc.edu>
2021-04-28 15:03:32 -0500

    Adding fake SSNs, zip codes, beginning to add typos

--- link-king-names/scribe-names.R
+++ link-king-names/scribe-names.R
@@ -8,6 +8,7 @@ rm(list=ls(all=TRUE)) #Clear the memory for any variables set from any previous 
 # ---- load-packages -----------------------------------------------------------
 library(magrittr                , quietly=TRUE)
 library(stringr                               )
+library(wakefield                             )
 requireNamespace("dplyr"                      )
 requireNamespace("lubridate"                  )
 requireNamespace("forcats"                    )
@@ -24,6 +25,35 @@ col_types = readr::cols_only(
     # X3             = readr::col_character(),
     # X4             = readr::col_character()
 )
+
+fake_ssn_with_repeats_function <- function(d){
+  # Specifically avoiding formats invalid as of 2011-06-25
+  d <-
+    d %>% 
+    dplyr::mutate(
+      area     = sample(c(1:665, 667:699, 729:899), dplyr::n(), replace = TRUE),
+      group    = sample(c(1:98)                   , dplyr::n(), replace = TRUE), # stopping short of 99, so I can safely increment by 1 if need be
+      serial   = sample(c(1:9999)                 , dplyr::n(), replace = TRUE),
+      
+      area_3   = stringr::str_pad(area  , 3, pad = "0"),
+      group_2  = stringr::str_pad(group , 2, pad = "0"),
+      serial_4 = stringr::str_pad(serial, 4, pad = "0"),
+      
+      ssn_fake  = paste(area_3, group_2, serial_4, sep = "-")
+    ) %>% 
+    dplyr::select(
+      -area,
+      -group,
+      -serial,
+      -area_3,
+      -group_2,
+      -serial_4
+    )
+  
+  
+  return(d)
+  
+}
 
 
 # ---- load-data ---------------------------------------------------------------
@@ -51,7 +81,7 @@ dsq2 <-
   dplyr::mutate(
     rown_o = 1:dplyr::n(),
   ) %>% 
-  dplyr::filter(rown_o <= 90000 ) %>%
+  dplyr::filter(rown_o <= 100 ) %>%
   dplyr::filter(!is.na(birth_year)) %>% 
   dplyr::group_by(rown_o) %>% 
   dplyr::mutate(
@@ -63,7 +93,10 @@ dsq2 <-
   dplyr::filter(count_name_segments <= 2) %>% 
   dplyr::mutate(
    name_middle = dplyr::lag(name_first, 1),
+   name_middle = dplyr::if_else(!is.na(name_middle), name_middle, dplyr::lead(name_first, 1)),
+   
    name_maiden = dplyr::lead(name_last, 1),
+   name_maiden = dplyr::if_else(!is.na(name_maiden), name_maiden, dplyr::lag(name_last, 1)),
    
    gender      = sample(0:1, dplyr::n(), replace = TRUE),
    gender      = dplyr::if_else(gender == 1, "Male", "Female"),
@@ -80,41 +113,41 @@ dsq2 <-
    birth_day   = sample(1:28, dplyr::n(), replace = TRUE),
    birth_month = sample(1:12, dplyr::n(), replace = TRUE),
    
-   dob         = paste(birth_year, birth_month, birth_day, sep = "-")
+   dob         = as.Date(paste(birth_year, birth_month, birth_day, sep = "-")),
+   
+   zip_code    = zip_code(n = dplyr::n(), k = dplyr::n())
+   
+  ) %>% 
+  fake_ssn_with_repeats_function() %>% 
+  dplyr::distinct(ssn_fake, .keep_all = TRUE) %>% 
+  dplyr::mutate(
+    rown_o          = as.character(rown_o),
+    rown_o_w        = paste0("w",rown_o),
+    
+    name_first_w    = name_first,
+    name_last_w     = name_last,
+    name_middle_w   = name_middle,
+    name_maiden_w   = name_maiden,
+    gender_w        = gender,
+    ethnicity_w     = ethnicity,
+    dob_w           = dob,
+    ssn_fake_w      = ssn_fake,
+    
+    name_middle_missing = as.logical(rbinom(n = dplyr::n(), size = 1, prob = 0.40)),
+    name_maiden_missing = as.logical(rbinom(n = dplyr::n(), size = 1, prob = 0.40)),
+    gender_missing      = as.logical(rbinom(n = dplyr::n(), size = 1, prob = 0.40)),
+    
+    gender_swapped      = as.logical(rbinom(n = dplyr::n(), size = 1, prob = 0.20)),
+    
+    name_middle_w = dplyr::if_else(name_middle_missing, NA_character_, name_middle_w),
+    name_maiden_w = dplyr::if_else(name_maiden_missing, NA_character_, name_maiden_w),
+    
+    gender_w      = dplyr::case_when(
+      !gender_swapped ~ gender_w,
+      gender_swapped & gender_w == "Male"   ~ "Female",
+      gender_swapped & gender_w == "Female" ~ "Male",
+    ),
+    
+    gender_w      = dplyr::if_else(gender_missing     , NA_character_, gender_w     ),
   )
 
-ds_ssn_test <-
-  dsq2 %>% 
-  dplyr::select(
-    rown_o
-  ) %>% 
-  dplyr::mutate(
-    area     = sample(c(1:665, 667:699, 729:899), dplyr::n(), replace = TRUE),
-    group    = sample(c(1:99)                   , dplyr::n(), replace = TRUE),
-    serial   = sample(c(1:9999)                 , dplyr::n(), replace = TRUE),
-    
-    area_3   = stringr::str_pad(area  , 3, pad = "0"),
-    group_2  = stringr::str_pad(group , 2, pad = "0"),
-    serial_4 = stringr::str_pad(serial, 4, pad = "0"),
-    
-    ssn_0    = paste(area_3, group_2, serial_4, sep = "-")
-  ) %>% 
-  dplyr::group_by(ssn_0) %>% 
-  dplyr::mutate(
-    ssn_count = dplyr::n()
-  ) %>% 
-  dplyr::ungroup() %>% 
-  dplyr::mutate(
-    not_unique = 1 < ssn_count,
-    swap       = sample(c(1:3), dplyr::n(), replace = TRUE)
-  )
-
-ds_ssn_test_2 <-
-  ds_ssn_test
-
-table(table(ds_ssn_test$ssn_0))
-table(ds_ssn_test$ssn_count)
-View(head(ds_ssn_test))
-
-table(table(ds_ssn_test$ssn_0))
-View(head(ds_ssn_test))
